b3b3e209ed03eb49712ad819da5a116c0898afe8
Dan Hill <danshill@gmail.com>
2024-01-11 15:05:34 -0700

    Lab complete

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function MainNav({ brand, leftLinks, rightLinks }) {
+  return (
+    <Navbar expand="lg" className="bg-success navbar-dark">
+      <Container fluid>
+      <Navbar.Brand href="\">{brand}</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            {leftLinks.map(({ url, text }, index) => (
+              <Nav.Link key={index} href={url}>
+                {text}
+              </Nav.Link>
+            ))}
+          </Nav>
+          <Nav>
+            {rightLinks.map(({ url, text }, index) => (
+              <Nav.Link key={index} href={url}>
+                {text}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
